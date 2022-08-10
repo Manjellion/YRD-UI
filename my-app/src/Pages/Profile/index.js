532b0d330303ca7d760352adf681051aafc35dc3
Manjil Ghale <rilleymanjil@hotmail.com>
2022-08-10 10:14:53 +0100

    Visual Improvement to add more context to the Profile page while remaining it a minimal design original appraoch

--- my-app/src/Pages/Profile/index.js
+++ my-app/src/Pages/Profile/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar'
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Logo from '../../Images/Logo/S_Logo.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import ProfileCard from '../../Components/ProfilePage/ProfileCard/ProfileCard'
 import Collection from '../../Components/ProfilePage/Collection/Main'
 import CSS from './index.module.css'
@@ -7,7 +11,21 @@ import CSS from './index.module.css'
 function index() {
   return (
     <div className={CSS.profile_container}>
-        <Navbar />
+        <div className={CSS.profile_navbar}>
+          <Navbar expand="lg" bg='dark' variant='dark'>
+          <Container>
+            <Navbar.Brand href="/" style={{ fontSize: 40 }}><Link to="/" style={{ textDecoration: 'none', color: '#fff' }}><img src={Logo} style={{ height: 75, width: 150 }} /></Link></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end">
+              <Nav.Link href='#Home'><Link to="/residentPage" style={{ textDecoration: 'none', color: '#fff'}}>Home</Link></Nav.Link>
+              <Nav.Link href='#Search' style={{color: '#fff'}}><Link to='/searchPage' style={{ textDecoration: 'none', color: '#fff'}}>Search</Link></Nav.Link>
+              <Nav.Link href='#Events' style={{color: '#fff'}}><Link to='/eventsPage' style={{ textDecoration: 'none', color: '#fff'}}>Events</Link></Nav.Link>
+              <Nav.Link href='#Contact' style={{color: '#fff'}}>Contact</Nav.Link>
+              <Nav.Link href='#Profile'><Link to='/login' style={{ textDecoration: 'none', color: '#fff' }}>Login</Link></Nav.Link>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>        
+        </div>
         <div className={CSS.profile_components}>
           <div>
             <ProfileCard />
